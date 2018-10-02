c787b0ac20e897fc5acbe7ed9511c8afc47f8926
Sony <Sony@Sony-VAIO>
2018-10-02 08:46:05 +0530

    created calculator script

---
+++ src/web/Calcpopup.java
@@ -0,0 +1,14 @@
+package web;
+
+import java.io.IOException;
+
+public class Calcpopup 
+{
+
+	public static void main(String[] args) throws IOException 
+	{
+		Runtime.getRuntime().exec("F:\\calc.exe");
+
+	}
+
+}
